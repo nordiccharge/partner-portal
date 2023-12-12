10c33536c818f63f0e286fc56a03caf6a39ae0e2
nordiccharge <sebastian@nordiccharge.com>
2023-12-12 16:35:19 +0100

    Testing API

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -23,7 +23,7 @@ class OrderController extends Controller
      */
     public function index(Request $request): \Illuminate\Http\JsonResponse
     {
-        return response()->json($request->server->get('team_id'));
+        return response()->json(dd($request->all()));
 
         if(!$this->apiAllowed($request)) {
             return response()->json('Unauthorized', 401);
