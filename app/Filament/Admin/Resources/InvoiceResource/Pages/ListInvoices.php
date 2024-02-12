59ec7a818d527f5a4333223496533fef26c709b1
nordiccharge <sebastian@nordiccharge.com>
2024-02-12 13:59:15 +0100

    Added tabs for invoices on admin panel

--- app/Filament/Admin/Resources/InvoiceResource/Pages/ListInvoices.php
+++ app/Filament/Admin/Resources/InvoiceResource/Pages/ListInvoices.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Admin\Resources\InvoiceResource\Pages;
 
+use App\Enums\InvoiceStatus;
 use App\Filament\Admin\Resources\InvoiceResource;
+use App\Models\Invoice;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListInvoices extends ListRecords
 {
@@ -15,5 +19,20 @@ class ListInvoices extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'unpaid' => Tab::make('Unpaid invoices')
+                ->badge(fn () => Invoice::all()->where('status', '!=', InvoiceStatus::Paid)->count())
+                ->modifyQueryUsing(function (Builder $query) {
+                    $query->where('status', '!=', InvoiceStatus::Paid);
+                }),
+            'paid' => Tab::make('Paid invoices')
+                ->modifyQueryUsing(function (Builder $query) {
+                    $query->where('status', InvoiceStatus::Paid);
+                }),
+            ];
     }
 }
