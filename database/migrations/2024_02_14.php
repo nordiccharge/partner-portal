352852af7cfa993009af9b242406351430a1ec4a
nordiccharge <sebastian@nordiccharge.com>
2024-02-14 08:31:57 +0100

    Fixing product_id unique

--- database/migrations/2024_02_14.php
+++ database/migrations/2024_02_14.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('inventories', function (Blueprint $table) {
+            $table->index('product_id');
             $table->dropUnique('inventories_product_id_unique');
         });
     }
