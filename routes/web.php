4cc37270dbd81467f9db62c30ee4ab786dd03ddf
nordiccharge <sebastian@nordiccharge.com>
2024-04-02 14:56:39 +0200

    Temporary fix

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect('/partner');
 });
-
-Route::get('/import', [\App\Http\Controllers\ImportController::class, 'import']);
