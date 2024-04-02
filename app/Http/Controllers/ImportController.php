c6aac34cec2dcbfa9eb47512ba6982c22e9d747a
nordiccharge <sebastian@nordiccharge.com>
2024-04-02 09:25:04 +0200

    Added temporary import of orders from customer

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -11,7 +11,7 @@ class ImportController extends Controller
         if (!$request->run) {
             return 'Please add ?run=true to the URL to run the import';
         }
-        $file = fopen('orders-1711035148.csv', 'r');
+        $file = fopen(database_path('data/orders-1711035148.csv'), 'r');
         echo '<p>Importing ' . $request->number . '</p>';
         $max = 1;
         if($request->number) {
