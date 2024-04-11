33cc6832b3d958d3838c9f82aead819b19da12c5
nordiccharge <sebastian@nordiccharge.com>
2024-04-11 15:57:34 +0200

    Fixes for pipeline

--- app/Filament/Admin/Resources/PipelineResource/RelationManagers/StagesRelationManager.php
+++ app/Filament/Admin/Resources/PipelineResource/RelationManagers/StagesRelationManager.php
@@ -26,7 +26,7 @@ class StagesRelationManager extends RelationManager
                 Forms\Components\Textarea::make('description')
                     ->string(),
                 Forms\Components\Select::make('order')
-                    ->options(range(1, 20))
+                    ->options([1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 7 => 7, 8 => 8, 9 => 9, 10 => 10, 11 => 11, 12 => 12, 13 => 13, 14 => 14, 15 => 15])
                     ->required(),
                 Forms\Components\Select::make('state')
                     ->options([
