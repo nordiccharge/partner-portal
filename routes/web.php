87a3eb93531b7ea77df29b4581fba2c8f3e0904a
nordiccharge <sebastian@nordiccharge.com>
2024-04-19 22:24:29 +0200

    Fixed admin redirect

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    if (auth()->check() && auth()->user()->is_admin) {
+    if (auth()->check() && auth()->user()->isAdmin()) {
         return redirect('/admin');
     }
     return redirect('/partner');
