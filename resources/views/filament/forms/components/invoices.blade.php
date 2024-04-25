9138c49fe925364af8a1e112bbfd97184294da18
nordiccharge <sebastian@nordiccharge.com>
2024-04-25 14:33:01 +0200

    Added new invoice functionality for admins

--- resources/views/filament/forms/components/invoices.blade.php
+++ resources/views/filament/forms/components/invoices.blade.php
@@ -1,6 +1,6 @@
 @if ($getRecord()->invoices->count() > 0)
     <div class="text-center">
-        <div style="--c-50:var(--danger-50);--c-400:var(--danger-400);--c-600:var(--danger-600);"
+        <div style="--c-50:var(--warning-50);--c-400:var(--warning-400);--c-600:var(--warning-600);"
              class="fi-badge flex items-center justify-center gap-x-1 rounded-md text-xs font-medium ring-1 ring-inset px-2 min-w-[theme(spacing.6)] py-1 fi-color-custom bg-custom-50 text-custom-600 ring-custom-600/10 dark:bg-custom-400/10 dark:text-custom-400 dark:ring-custom-400/30">
             <span class="grid">
                 <span class="truncate">
