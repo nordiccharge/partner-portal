52a16a5d0b8b47c92a1912c623c96d0d8d464f09
nordiccharge <sebastian@nordiccharge.com>
2024-05-02 15:01:03 +0200

    Updated routing for API

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::group(['domain' => 'portal.nordiccharge.com'], function(){
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::apiResource('orders', \App\Http\Controllers\API\OrderController::class);
-Route::apiResource('products', \App\Http\Controllers\API\ProductController::class);
-Route::get('pipelines', [\App\Http\Controllers\API\PipelineController::class, 'index']);
-Route::get('pipelines/{id}', [\App\Http\Controllers\API\PipelineController::class, 'show']);
-Route::get('inventory', [\App\Http\Controllers\API\InventoryController::class, 'index']);
-Route::get('installations', [\App\Http\Controllers\API\InstallationController::class, 'index']);
-Route::post('shipping', [\App\Http\Controllers\API\ShipmentController::class, 'store']);
-Route::post('date', [\App\Http\Controllers\API\InstallationDateController::class, 'index']);
+    Route::apiResource('orders', \App\Http\Controllers\API\OrderController::class);
+    Route::apiResource('products', \App\Http\Controllers\API\ProductController::class);
+    Route::get('pipelines', [\App\Http\Controllers\API\PipelineController::class, 'index']);
+    Route::get('pipelines/{id}', [\App\Http\Controllers\API\PipelineController::class, 'show']);
+    Route::get('inventory', [\App\Http\Controllers\API\InventoryController::class, 'index']);
+    Route::get('installations', [\App\Http\Controllers\API\InstallationController::class, 'index']);
+    Route::post('shipping', [\App\Http\Controllers\API\ShipmentController::class, 'store']);
+    Route::post('date', [\App\Http\Controllers\API\InstallationDateController::class, 'index']);
+
+});
